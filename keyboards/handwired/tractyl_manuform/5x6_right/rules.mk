df0a65bf7ee655cfbffd43b934969f99cd55d088
caelan-taiv <caelan@taiv.tv>
2025-05-16 07:53:48 -0500

    Update rules.mk to match custom tractyl repo

--- keyboards/handwired/tractyl_manuform/5x6_right/rules.mk
+++ keyboards/handwired/tractyl_manuform/5x6_right/rules.mk
@@ -1,4 +1,5 @@
+LTO_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
 MOUSE_SHARED_EP = yes
 
-DEFAULT_FOLDER = handwired/tractyl_manuform/5x6_right/teensy2pp
+DEFAULT_FOLDER = handwired/tractyl_manuform/5x6_right/arduinomicro
